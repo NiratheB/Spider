51cf1e62304a70ad3377e8ea24b8ee34c3d00a53
behtarin <behtarinferdousi@gmail.com>
2018-09-10 19:15:16 +0200

    Finds all the side links (genres) in the book list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Document page = Jsoup.connect("http://books.toscrape.com/").timeout(6000).get();
+            Elements links = page.select("div.side_categories a");
+            for(Element link : links){
+                System.out.println(link.text());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
